92624c2b254124db5a5c27dd3c21a993815ba5ef
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-03-11 15:43:19 -0300

    Mostrando o estado Deleted e como o Entity gerencia o SQL

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -23,14 +23,18 @@ namespace Alura.Loja.Testes.ConsoleApp
                 var produtos = contexto.Produtos.ToList();
                 ExibeEntries(contexto.ChangeTracker.Entries());
 
-                var novoProduto = new Produto
-                {
-                    Nome = "Desinfetante",
-                    Categoria = "Limpeza",
-                    Preco = 2.99
-                };
+                //var novoProduto = new Produto
+                //{
+                //    Nome = "Desinfetante",
+                //    Categoria = "Limpeza",
+                //    Preco = 2.99
+                //};
 
-                contexto.Produtos.Add(novoProduto);
+                //contexto.Produtos.Add(novoProduto);
+
+                var p1 = produtos.First();
+                contexto.Produtos.Remove(p1);
+
                 ExibeEntries(contexto.ChangeTracker.Entries());
 
                 contexto.SaveChanges();
