a197ec62f74dfd203a3ed13dd78997b1ef4fa9ae
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-03-11 20:41:04 -0300

    Criando classe de compra 1:N com produtos

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,7 +14,30 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            //compra de 6 pães fraceses
+            var paoFrances = new Produto
+            {
+                Nome = "Pão Francês",
+                PrecoUnitario = 0.40,
+                Unidade = "Unidade",
+                Categoria = "Padaria"
+            };
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using(var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                contexto.Compras.Add(compra);
+
+                contexto.SaveChanges();
+            }
         }
     }
 }
